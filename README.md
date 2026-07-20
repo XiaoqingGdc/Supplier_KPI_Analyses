02bc0f108f34c280b44cd7f3ea68eb2a207b3acd
Xiaoqing <xiaoqing.zhou@outlook.fr>
2026-07-20 20:16:01 +0200

    Fix directory structure

--- README.md
+++
@@ -1,4 +0,0 @@
-#mon projet du vendredi 03 juillet 2026
-#la suite arrive👌
-# nice 👍
-# good ![alt text](image.png)
--- analyse.py
+++
@@ -1,8 +0,0 @@
-def calcul_moyenne(notes):
-    total = 0
-    for note in notes:
-        total+=note
-    return total/len(notes)
-notes=[5,12,52]
-
-print(calcul_moyenne(notes))
